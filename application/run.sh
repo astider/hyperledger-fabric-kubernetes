cba4665245aa89843927b83fc372ed26c825f11e
nontapat <astider@gmail.com>
2020-09-28 11:33:10 +0700

    finish deployment process, create yaml for both staging and production

--- application/run.sh
+++ application/run.sh
@@ -4,11 +4,18 @@ echo "Node.js version: $NODE_VERSION"
 cd $APPLICATION_PATH
 npm install
 sleep 10
+echo ""
 echo "Creating Wallet..."
 node enrollAdmin.js
-node registerUser.js
+echo "Enrolled Admin."
+echo ""
+WALLET_USER_ID=$(date +%s)
+echo "Registering user: $WALLET_USER_ID"
+node registerUser.js $WALLET_USER_ID
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
 echo "Wallet (should be successfully) created."
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
 sleep 5
 echo "Starting the server..."
-node server.js
+node server.js $WALLET_USER_ID
--- application/server.js
+++ application/server.js
@@ -10,6 +10,9 @@ const PORT = 8888;
 const add = require('./invoke');
 const query = require('./query');
 
+const idNumber = process.argv[2];
+const userId = `user_${idNumber}`;
+
 app.use((req, res, next) => {
   res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
   res.header('Expires', '-1');
@@ -18,7 +21,8 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  req.user = 'user260917';
+  // req.user = 'user260917';
+  req.user = userId
   next();
 })
 
